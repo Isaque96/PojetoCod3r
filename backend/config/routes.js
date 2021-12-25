e92b920cfd1834cc932fd56e407185f45c76729c
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-12-24 21:23:04 -0300

    GetTree Metohd Created

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,5 +5,8 @@ module.exports = app => {
 
   app.route('/categories').get(app.api.category.get).post(app.api.category.save);
 
+  // Cuidado com a Ordem, aqui ela importa - Quando ele escuta o path com algum parametro tem que ser sempre passado depois do path normal que não é parametro - Ex.: tree antes de :id
+  app.route('/categories/tree').get(app.api.category.getTree);
+
   app.route('/categories/:id').get(app.api.category.getById).put(app.api.category.save).delete(app.api.category.remove);
 }
