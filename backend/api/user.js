9c21cc4d504ba7df16b959f5adec9db9428503d8
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-12-26 18:36:44 -0300

    Delete User Method

--- backend/api/user.js
+++ backend/api/user.js
@@ -49,6 +49,7 @@ module.exports = app => {
         .where({
           id: user.id
         })
+        .whereNull('deletedAt')
         .then(_ => res.status(204).send())
         .catch(err => res.status(500).send(err))
     } else {
@@ -62,6 +63,7 @@ module.exports = app => {
   const get = (req, res) => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
+      .whereNull('deletedAt')
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
@@ -69,15 +71,42 @@ module.exports = app => {
   const getById = (req, res) => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
-      .where({ id: req.params.id })
+      .where({
+        id: req.params.id
+      })
+      .whereNull('deletedAt')
       .first()
       .then(user => res.json(user))
       .catch(err => res.status(500).send(err))
   }
 
+  const remove = async (req, res) => {
+    try {
+      const articles = await app.db('articles')
+        .where({
+          userId: req.params.id
+        })
+      notExistsOrError(articles, 'Usuário possui artigos.')
+
+      const rowsUpdated = await app.db('users')
+        .update({
+          deletedAt: new Date()
+        })
+        .where({
+          id: req.params.id
+        })
+      existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+
+      res.status(204).send()
+    } catch (msg) {
+      res.status(400).send(msg)
+    }
+  }
+
   return {
     save,
     get,
-    getById
+    getById,
+    remove
   }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -7,7 +7,7 @@ module.exports = app => {
 
   app.route('/users').all(app.config.passport.authenticate()).post(admin(app.api.user.save)).get(admin(app.api.user.get));
 
-  app.route('/users/:id').all(app.config.passport.authenticate()).put(admin(app.api.user.save)).get(admin(app.api.user.getById));
+  app.route('/users/:id').all(app.config.passport.authenticate()).put(admin(app.api.user.save)).get(admin(app.api.user.getById)).delete(admin(app.api.user.remove));
 
   app.route('/categories').all(app.config.passport.authenticate()).get(admin(app.api.category.get)).post(admin(app.api.category.save));
 
