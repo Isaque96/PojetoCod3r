7a07bee0d4e66f1dfd070b08cb1b535915328aa5
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-01-14 10:49:45 -0300

    Accessing BackEnd and Fixed Conflict with Node 17

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Problema de reconhecimento no Node trocar de "localhost" para o IP 127.0.0.1
+mongoose.connect('mongodb://127.0.0.1/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(e => {
     const msg = 'Não foi possível conectar com MongoDB!' + e;
     console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
