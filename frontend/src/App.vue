0b609439cc229625e350f54252c0191b4872c818
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-01-16 21:06:06 -0300

    Created Validate

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,15 +9,51 @@
 
 <script>
 import { mapState } from 'vuex';
+import { baseApiUrl, userKey } from '@/global';
 import Header from '@/components/template/Header.vue';
 import Menu from '@/components/template/Menu.vue';
 import Content from '@/components/template/Content.vue';
 import Footer from '@/components/template/Footer.vue';
+import axios from 'axios';
 
 export default {
 	name: "App",
 	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible', 'user'])
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true;
+
+			const json = localStorage.getItem(userKey);
+			const userData = JSON.parse(json);
+			this.$store.commit('setUser', null);
+
+			if(!userData) {
+				this.validatingToken = false;
+				this.$router.push({ name: 'auth' });
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData);
+
+			if(res.data) {
+				this.$store.commit('setUser', userData);
+			} else {
+				localStorage.removeItem(userKey);
+				this.$router.push({ name: 'auth' });
+			}
+
+			this.validatingToken = false;
+		},
+		created() {
+			this.validateToken();
+		}
+	}
 }
 </script>
 
